d2b4ea96da74542e089966431ffa7a51971f2517
riottrainingbase-art <riot.training.base@gmail.com>
2026-09-24 21:28:40 +0900

    Remove unused HOMECOURT registration import

--- components/my-homecourt.tsx
+++ components/my-homecourt.tsx
@@ -1,5 +1,5 @@
 import { ArrowRight, ArrowUpRight, BookOpen, CalendarDays, Check, Compass, CreditCard, FileText, HeartHandshake, History, House, LifeBuoy, LockKeyhole, MessageCircle, Sparkles, Users } from "lucide-react";
-import { COACH_COMMUNITY_URL, homecourtFreeRegistrationUrl, HomecourtRole, PARENT_COMMUNITY_URL, homecourtRoles } from "./homecourt-data";
+import { COACH_COMMUNITY_URL, HomecourtRole, PARENT_COMMUNITY_URL, homecourtRoles } from "./homecourt-data";
 import { Locale, localePath, SiteFrame } from "./site-frame";
 
 const copy={
