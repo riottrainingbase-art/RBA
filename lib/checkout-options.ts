42d6975392371d3370f690828a82b879e417ca6b
riottrainingbase-art <riot.training.base@gmail.com>
2026-09-24 21:26:47 +0900

    Add HOMECOURT monthly checkout route

--- lib/checkout-options.ts
+++ lib/checkout-options.ts
@@ -1,4 +1,5 @@
 export const checkoutOffers: Readonly<Record<string,string>> = Object.freeze({
+ "homecourt-monthly":"homecourt-monthly",
  "torsten-live":"torsten-live-vol2","torsten-ondemand":"torsten-ondemand-vol2",
  "saga-fukuoka-2day":"saga-fukuoka-2026","yamagata-1day":"yamagata-1day-2026","shizugawa-2day":"shizugawa-2026",
  "kobe-half-friday":"kobe-half-friday-2026","kobe-1day-friday":"kobe-one-friday-2026","kobe-2day-friday":"kobe-two-friday-2026","kobe-3day-friday":"kobe-three-friday-2026","kobe-2day-stay-friday":"kobe-2d1n-friday-2026","kobe-3day-stay-friday":"kobe-full-friday-2026"
