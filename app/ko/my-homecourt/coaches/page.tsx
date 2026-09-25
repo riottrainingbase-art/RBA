125f363e4997819133445a17092d5b118ea4c93f
riottrainingbase-art <riot.training.base@gmail.com>
2026-09-25 21:19:49 +0900

    Add ko coaches MY HOME COURT metadata

--- app/ko/my-homecourt/coaches/page.tsx
+++ app/ko/my-homecourt/coaches/page.tsx
@@ -1,2 +1,4 @@
 import { MyHomecourt } from "@/components/my-homecourt";
+import { homecourtRoleMetadata } from "@/components/page-metadata";
+export const metadata=homecourtRoleMetadata("ko","coaches");
 export default function Page(){return <MyHomecourt locale="ko" role="coaches"/>}
