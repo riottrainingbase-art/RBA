469b9c12299597ac457f20cbe75b5a66b9b6092d
riottrainingbase-art <riot.training.base@gmail.com>
2026-09-25 21:18:53 +0900

    Add ja players MY HOME COURT metadata

--- app/ja/my-homecourt/players/page.tsx
+++ app/ja/my-homecourt/players/page.tsx
@@ -1,2 +1,4 @@
 import { MyHomecourt } from "@/components/my-homecourt";
+import { homecourtRoleMetadata } from "@/components/page-metadata";
+export const metadata=homecourtRoleMetadata("ja","players");
 export default function Page(){return <MyHomecourt locale="ja" role="players"/>}
