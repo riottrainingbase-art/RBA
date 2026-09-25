5b46edad160db5d376137118baaba07702c72810
riottrainingbase-art <riot.training.base@gmail.com>
2026-09-25 21:18:45 +0900

    Add en coaches MY HOME COURT metadata

--- app/my-homecourt/coaches/page.tsx
+++ app/my-homecourt/coaches/page.tsx
@@ -1,2 +1,4 @@
 import { MyHomecourt } from "@/components/my-homecourt";
+import { homecourtRoleMetadata } from "@/components/page-metadata";
+export const metadata=homecourtRoleMetadata("en","coaches");
 export default function Page(){return <MyHomecourt locale="en" role="coaches"/>}
